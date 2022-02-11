f6b56bb722dc387698f1ab6dede9b1d5aa5b565f
blakeian0 <buddinghb@macstudent.com.au>
2022-02-11 11:45:48 +1100

    Initial Commit

---
+++ StudyBot/StudyBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBotApp.swift
+//  StudyBot
+//
+//  Created by Blake Buddingh on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct StudyBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
