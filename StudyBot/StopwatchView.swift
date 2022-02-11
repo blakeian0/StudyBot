e02e9e405b8bf957161df9e1802c8b78c8d1e38a
blakeian0 <buddinghb@macstudent.com.au>
2022-02-11 14:20:04 +1100

    file changes

---
+++ StudyBot/StopwatchView.swift
@@ -0,0 +1,20 @@
+//
+//  StopwatchView.swift
+//  StudyBot
+//
+//  Created by Blake Buddingh on 11/2/22.
+//
+
+import SwiftUI
+
+struct StopwatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StopwatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        StopwatchView()
+    }
+}
