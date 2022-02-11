40c7101b34f5aafbcd8b5345331ed00d735431cc
blakeian0 <buddinghb@macstudent.com.au>
2022-02-11 14:39:00 +1100

    stopwatch non functional

--- StudyBot/StopwatchView.swift
+++ StudyBot/StopwatchView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct StopwatchView: View {
+    ///Current Progress time expressed in seconds
+    @State private var progressTime = 0
+    
+    ///Computing properties
+    var hours: Int {
+        progressTime / 3600
+    }
+    
+    var minutes: Int {
+      (progressTime % 3600) / 60
+    }
+
+    var seconds: Int {
+      progressTime % 60
+    }
+    
+    /// Increase progressTime each second
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            progressTime += 1
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Stopwatch")
+        Text("\(hours):\(minutes):\(seconds)")
     }
 }
 
