5c7fb719676aa2ece953570a0903fa801fa87ac5
blakeian0 <buddinghb@macstudent.com.au>
2022-02-15 23:28:52 +1100

    working animation, added button and accentcolor setting

--- StudyBot/StopwatchView.swift
+++ StudyBot/StopwatchView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct StopwatchView: View {
     ///Current Progress time expressed in seconds
-    @State var startingTime = 25 * 60
-    @State var countdownTimer = 25 * 60
+    @State var startingTime = 5 * 60
+    @State var countdownTimer = 5 * 60
     @State var timerRunning = false
+    @State var progress: CGFloat = 0
+    
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
+    
     var minutes: String {
         String((countdownTimer % 3600) / 60)
     }
@@ -28,14 +31,22 @@ struct StopwatchView: View {
     
     var body: some View {
         VStack {
-            Text("\(minutes):\(seconds)").onReceive(timer) { _ in
-                if countdownTimer > 0 && timerRunning {
-                    countdownTimer -= 1
-                } else {
-                    timerRunning = false
+            ZStack{
+                Text("\(minutes):\(seconds)").onReceive(timer) { _ in
+                    if countdownTimer > 0 && timerRunning {
+                        countdownTimer -= 1
+                        withAnimation() {
+                            progress = 1 - CGFloat(Float(countdownTimer) / Float(startingTime))
+                        }
+                    } else {
+                        timerRunning = false
+                    }
                 }
+                .font(.system(size: 90))
+                
+                CircularProgressBar(circleProgress: $progress, widthAndHeight: 300, staticColor: .accentColor, progressColor: .gray, showLabel: false)
             }
-            .font(.system(size: 90))
+
             
             
             HStack(spacing: 30) {
@@ -45,10 +56,26 @@ struct StopwatchView: View {
                 
                 Button("Reset") {
                     countdownTimer = startingTime
+                    progress = 0
                 }
             }
             
-            Text("\(countdownTimer)")
+            Text("\(countdownTimer), \(progress)")
+            
+            Button(action: {
+                print("Round Action")
+                }) {
+                    ZStack {
+                        Circle()
+                            .stroke(Color.accentColor, lineWidth: 3)
+                            .frame(width: 110, height: 110)
+                        Text("Press")
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(Color.black)
+                            .background(Color.accentColor)
+                            .clipShape(Circle())
+                    }
+            }
         }
     }
 }
